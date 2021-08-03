7d14bbc9dcfa1919d13d9eac8f2e91cd3a89f72e
Rupayan Munshi <rupayanmain.08@gmail.com>
2021-08-04 04:10:57 +0530

    googleLogin and firestore initiallisation p.s. update security

--- src/pages/Loginsignup.js
+++ src/pages/Loginsignup.js
@@ -1,13 +1,28 @@
-import Signup from "../components/Signup"
-import Signin from "../components/Signin"
-import { BrowserRouter as Router,Switch , Route } from "react-router-dom"
-const Loginsignup = ()=>{
-    return (
-    <Router>
-        <Switch>
-        <Route path="/" exact component={Signin}/>
-        <Route path="/signup" exact component={Signup}/>
-        </Switch>
-    </Router>)
-}
-export default Loginsignup
+import fire from "../firebase";
+import firebase from "firebase";
+import "firebase/auth";
+import newUser from "../firestore";
+const Loginsignup = () => {
+  const googleAuth = async (provider) => {
+    return fire
+      .auth()
+      .signInWithPopup(provider)
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+  const loginHandler = async () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    const authentication = googleAuth(googleProvider);
+    authentication
+      .then((res) => fire.auth().signInWithCredential(res.credential))
+      .then((res) => {
+        newUser(res.user.uid);
+      });
+  };
+  return <button onClick={() => loginHandler()}>google</button>;
+};
+export default Loginsignup;
