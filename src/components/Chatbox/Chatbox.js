0efff916c2bdfd8b3a605a2992566a39d1b26730
Rupayan Munshi <rupayanmain.08@gmail.com>
2021-09-02 13:24:09 +0530

    profile page

--- src/components/Chatbox/Chatbox.js
+++ src/components/Chatbox/Chatbox.js
@@ -5,39 +5,80 @@ import Sidebar from "./Sidebar";
 import Activeroom from "./Activeroom";
 import Header from "./Header/Header";
 import "./chatbox.css";
-import Profile from "../../pages/Profile";
-
+import Profile from "../../pages/othersProfile";
+import MyProfile from "../../pages/myProfile";
 import { db } from "../../firestore";
 
-const users = db.collection("users");
-
-const Chatbox = ({ signOutHandler }) => {
+const Chatbox = ({ signOutHandler, user }) => {
+  const users = db.collection("users");
+  const [myData, setMyData] = useState({ friendRequest: {} });
   const [rooms, setRooms] = useState([]);
   const [room, setRoom] = useState(null);
   const [messages, setMessages] = useState([]);
   const scrollRef = useRef(null);
-  const user = "Subhajit";
+  const [myProfile, setMyProfile] = useState(false);
   const [profile, setProfile] = useState(false);
-
+  useEffect(() => {
+    db.collection("users")
+      .doc(user.email)
+      .get()
+      .then((querySnapshot) => {
+        setMyData(querySnapshot.data());
+      });
+  }, []);
   return (
     <div>
-      <div>
-        <Grid container className="container">
-          <Grid item xs={12}>
-            <Header dummyData={dummyData} signOutHandler={signOutHandler} />
+      {profile === false && myProfile == false ? (
+        <div>
+          <Grid container className="container">
+            <Grid item xs={12}>
+              <Header dummyData={dummyData} />
+              <button
+                onClick={signOutHandler}
+                style={{ width: "100%", height: "100%", color: "white" }}
+              ></button>
+            </Grid>
+            <Grid item xs={3} className="sidebar">
+              <Sidebar
+                user={user}
+                dummyData={dummyData}
+                setProfile={setProfile}
+              ></Sidebar>
+            </Grid>
+            <Grid item xs className="chat-box">
+              <Activeroom
+                user={user}
+                message={dummyMessages}
+                room={room}
+              ></Activeroom>
+            </Grid>
           </Grid>
-          <Grid item xs={3} className="sidebar">
-            <Sidebar dummyData={dummyData} setProfile={setProfile}></Sidebar>
-          </Grid>
-          <Grid item xs className="chat-box">
-            <Activeroom
-              user={user}
-              message={dummyMessages}
-              room={room}
-            ></Activeroom>
-          </Grid>
-        </Grid>
-      </div>
+          <button
+            style={{
+              width: "1000px",
+              height: "100px",
+              backgroundColor: "green",
+            }}
+            onClick={() => setMyProfile(true)}
+          ></button>
+        </div>
+      ) : profile ? (
+        <Profile
+          profile={profile}
+          user={user}
+          setProfile={setProfile}
+          myData={myData}
+          setMyData={setMyData}
+        />
+      ) : (
+        <MyProfile
+          user={user}
+          setMyProfile={setMyProfile}
+          setProfile={setProfile}
+          myData={myData}
+          setMyData={setMyData}
+        />
+      )}
     </div>
   );
 };
--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Chatbox from "../components/Chatbox/Chatbox";
 
-const Chat = ({ signOutHandler, setProfile }) => {
+const Chat = ({ signOutHandler, setProfile, user, allUserData }) => {
   return (
     <div>
-      <Chatbox signOutHandler={signOutHandler} setProfile={setProfile} />
+      <Chatbox
+        signOutHandler={signOutHandler}
+        setProfile={setProfile}
+        user={user}
+      />
     </div>
   );
 };
-
 export default Chat;
